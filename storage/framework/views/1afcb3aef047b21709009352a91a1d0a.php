5d1be7561393cfe35bca30bdb625fa1a1140b3e8
Muhammad Fajar Fikri Fadilah <fajarfikri31@gmail.com>
2023-12-18 15:34:17 +0700

    update

---
+++ storage/framework/views/1afcb3aef047b21709009352a91a1d0a.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+</head>
+
+<body class="">
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+</body>
+
+</html>
+<?php /**PATH D:\mfajarfikri\Test\FLTower\resources\views/layouts/layout.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/cb1ddb03bbcbd6316576383292c673e6.php
@@ -0,0 +1,42 @@
+<?php echo $__env->make('component.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php $__env->startSection('content'); ?>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ea iusto dolorum, provident praesentium ad corrupti
+        aliquam dolorem fugit! Placeat, sit quae! Expedita sit nihil nam. Quae, laudantium reprehenderit quod molestias
+        sequi sint vel assumenda necessitatibus, debitis fugiat repellendus ex voluptatibus provident possimus mollitia
+        nulla. Ad voluptatem accusamus mollitia. Adipisci natus quaerat, blanditiis incidunt reprehenderit ut consectetur
+        vel eveniet eum error accusantium beatae voluptatum! Assumenda quae repellendus nemo in. Dolore similique est qui
+        sapiente, obcaecati quod reprehenderit ipsam fugiat non repellendus debitis rem ab modi explicabo saepe distinctio
+        atque necessitatibus numquam, officiis recusandae tempore at iusto omnis! Distinctio ad mollitia, culpa commodi
+        expedita eligendi quis, dolorem sequi fuga necessitatibus modi odit, est minima eum amet voluptate unde iusto illum
+        deserunt adipisci. Quo placeat obcaecati, expedita, laborum eveniet officia numquam nihil maxime tempore amet vero
+        eligendi deserunt? Exercitationem sed consequatur, odio omnis iusto rem? Error ipsa eaque dolore quaerat doloremque
+        unde quos nam magnam necessitatibus distinctio repudiandae eos facere, mollitia iste fuga saepe hic? Fugiat possimus
+        odit eligendi. Aliquam saepe quisquam quos. Iusto aliquam voluptas aliquid officiis tempore quidem, quo vitae,
+        dolores cum exercitationem nam alias totam temporibus obcaecati dolorem inventore. Tempore ipsum odit possimus animi
+        maiores at qui nesciunt, numquam minus. Vel deserunt consequuntur sit error nobis aut magnam aspernatur doloremque,
+        ullam distinctio, culpa architecto. Delectus nulla ad dolorum. Est maiores repellendus minus rerum voluptates a
+        perspiciatis dignissimos voluptas? Minus, iusto perspiciatis? Nisi officia, quam tempore labore culpa voluptatum
+        consequatur laudantium, debitis blanditiis quia eveniet inventore alias reiciendis mollitia modi aliquam distinctio
+        at incidunt et? Vitae sint illum cum minus ex, temporibus eum, alias quas, qui rem et ratione porro maiores
+        accusantium necessitatibus ut nostrum ad nemo tempore omnis eos? Odio rem repudiandae autem similique quisquam
+        voluptas! Quisquam deleniti aut perspiciatis aliquam consequatur reiciendis blanditiis perferendis eum esse rerum
+        praesentium assumenda nulla, unde voluptates vitae ut tempore repudiandae nihil et dolores fugiat ex a harum ab.
+        Voluptatibus dicta aliquid autem voluptatum quaerat veniam aspernatur iusto laborum repellat alias totam quibusdam
+        nesciunt ipsum ullam dignissimos nulla quod, delectus ab consequatur laudantium obcaecati. Hic aliquam vitae qui
+        ipsam a, assumenda ullam nostrum provident laborum nobis modi reiciendis eveniet ex, cum alias soluta. Commodi
+        consequuntur soluta natus in eveniet facilis nulla asperiores nostrum fugiat modi, perspiciatis itaque aspernatur,
+        maxime blanditiis ea id excepturi enim minus alias adipisci, iste facere consequatur. Reiciendis atque esse
+        laudantium fuga placeat maiores voluptate? Labore iusto alias, accusantium ad asperiores porro placeat reiciendis
+        quo optio minus unde dolorem aliquam natus esse consequuntur ratione, veniam provident obcaecati mollitia hic modi
+        quisquam beatae. Numquam eligendi laudantium praesentium, dolor natus eius cumque, quisquam, asperiores enim et hic
+        deserunt illum voluptatibus! Itaque sit recusandae cumque exercitationem, eos laborum amet quia esse. Enim amet,
+        quis magni sapiente quas numquam maxime fugiat neque expedita error fugit consequatur aliquid consequuntur, labore,
+        non itaque natus iusto possimus harum porro ipsum consectetur. Dolorem cum consectetur tempora nostrum, et ut ea
+        corrupti doloremque error, fugit adipisci libero eius impedit quaerat repellendus quos illo, sapiente saepe possimus
+        quae alias debitis!</h1>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\mfajarfikri\Test\FLTower\resources\views/home.blade.php ENDPATH**/ ?>
