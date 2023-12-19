1914ed3afd25e0df103608408671b38bb74f31b8
Muhammad Fajar Fikri Fadilah <fajarfikri31@gmail.com>
2023-12-19 08:22:34 +0700

    add feature dashboard

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+
+@section('content')
+    <H1>Dashboard</H1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(HomeController::class)->group(function(){
+    Route::get('/', 'index');
 });
 
-Route::get('/', function () {
-    return view('home');
+Route::controller(DashboardController::class)->group(function(){
+    Route::get('dashboard', 'index');
 });
