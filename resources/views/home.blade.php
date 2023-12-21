e41088aa72d3e2f449a3b2939103a16d075a1097
Muhammad Fajar Fikri Fadilah <fajarfikri31@gmail.com>
2023-12-21 09:35:44 +0700

    update feature dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,38 +3,22 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ea iusto dolorum, provident praesentium ad corrupti
-        aliquam dolorem fugit! Placeat, sit quae! Expedita sit nihil nam. Quae, laudantium reprehenderit quod molestias
-        sequi sint vel assumenda necessitatibus, debitis fugiat repellendus ex voluptatibus provident possimus mollitia
-        nulla. Ad voluptatem accusamus mollitia. Adipisci natus quaerat, blanditiis incidunt reprehenderit ut consectetur
-        vel eveniet eum error accusantium beatae voluptatum! Assumenda quae repellendus nemo in. Dolore similique est qui
-        sapiente, obcaecati quod reprehenderit ipsam fugiat non repellendus debitis rem ab modi explicabo saepe distinctio
-        atque necessitatibus numquam, officiis recusandae tempore at iusto omnis! Distinctio ad mollitia, culpa commodi
-        expedita eligendi quis, dolorem sequi fuga necessitatibus modi odit, est minima eum amet voluptate unde iusto illum
-        deserunt adipisci. Quo placeat obcaecati, expedita, laborum eveniet officia numquam nihil maxime tempore amet vero
-        eligendi deserunt? Exercitationem sed consequatur, odio omnis iusto rem? Error ipsa eaque dolore quaerat doloremque
-        unde quos nam magnam necessitatibus distinctio repudiandae eos facere, mollitia iste fuga saepe hic? Fugiat possimus
-        odit eligendi. Aliquam saepe quisquam quos. Iusto aliquam voluptas aliquid officiis tempore quidem, quo vitae,
-        dolores cum exercitationem nam alias totam temporibus obcaecati dolorem inventore. Tempore ipsum odit possimus animi
-        maiores at qui nesciunt, numquam minus. Vel deserunt consequuntur sit error nobis aut magnam aspernatur doloremque,
-        ullam distinctio, culpa architecto. Delectus nulla ad dolorum. Est maiores repellendus minus rerum voluptates a
-        perspiciatis dignissimos voluptas? Minus, iusto perspiciatis? Nisi officia, quam tempore labore culpa voluptatum
-        consequatur laudantium, debitis blanditiis quia eveniet inventore alias reiciendis mollitia modi aliquam distinctio
-        at incidunt et? Vitae sint illum cum minus ex, temporibus eum, alias quas, qui rem et ratione porro maiores
-        accusantium necessitatibus ut nostrum ad nemo tempore omnis eos? Odio rem repudiandae autem similique quisquam
-        voluptas! Quisquam deleniti aut perspiciatis aliquam consequatur reiciendis blanditiis perferendis eum esse rerum
-        praesentium assumenda nulla, unde voluptates vitae ut tempore repudiandae nihil et dolores fugiat ex a harum ab.
-        Voluptatibus dicta aliquid autem voluptatum quaerat veniam aspernatur iusto laborum repellat alias totam quibusdam
-        nesciunt ipsum ullam dignissimos nulla quod, delectus ab consequatur laudantium obcaecati. Hic aliquam vitae qui
-        ipsam a, assumenda ullam nostrum provident laborum nobis modi reiciendis eveniet ex, cum alias soluta. Commodi
-        consequuntur soluta natus in eveniet facilis nulla asperiores nostrum fugiat modi, perspiciatis itaque aspernatur,
-        maxime blanditiis ea id excepturi enim minus alias adipisci, iste facere consequatur. Reiciendis atque esse
-        laudantium fuga placeat maiores voluptate? Labore iusto alias, accusantium ad asperiores porro placeat reiciendis
-        quo optio minus unde dolorem aliquam natus esse consequuntur ratione, veniam provident obcaecati mollitia hic modi
-        quisquam beatae. Numquam eligendi laudantium praesentium, dolor natus eius cumque, quisquam, asperiores enim et hic
-        deserunt illum voluptatibus! Itaque sit recusandae cumque exercitationem, eos laborum amet quia esse. Enim amet,
-        quis magni sapiente quas numquam maxime fugiat neque expedita error fugit consequatur aliquid consequuntur, labore,
-        non itaque natus iusto possimus harum porro ipsum consectetur. Dolorem cum consectetur tempora nostrum, et ut ea
-        corrupti doloremque error, fugit adipisci libero eius impedit quaerat repellendus quos illo, sapiente saepe possimus
-        quae alias debitis!</h1>
+    <div class="mx-10 my-10 cursor-pointer border-solid border-gray-900">
+        <div class="px-5 py-3 bg-white rounded-lg">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet et sequi aliquam laboriosam magni quia.
+                Delectus,
+                consequatur voluptatum iusto natus alias perferendis. Modi laborum voluptates asperiores quaerat rem
+                nesciunt
+                maiores magni dolor, labore adipisci minima rerum doloribus officia? Quae perspiciatis ipsum beatae facilis
+                autem iste reprehenderit officia pariatur fugit sequi quibusdam alias, doloribus eius facere distinctio
+                expedita
+                repellat, ratione accusamus amet vero ex atque! Modi soluta aliquid numquam dolorum qui nesciunt, reiciendis
+                repudiandae nam est amet architecto ex aliquam praesentium voluptatibus cupiditate! Iusto porro excepturi
+                expedita ipsam? Reprehenderit voluptate numquam voluptatem in sunt eligendi, ab fugit error sit, mollitia
+                voluptatibus.</h1>
+        </div>
+    </div>
+
+
+    <div id="map" class=""></div>
 @endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,14 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="shortcut icon" href="{{ asset('image/PLN.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('image/favicon.ico') }}" type="ico">
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+
+
+
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 
-<body class="">
+<body class="min-h-screen bg-[#eceff180]">
 
     @yield('content')
 
+
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 </body>
 
 </html>
